18d389b0a1f06887ff7ab89f33c05e567ef4656b
Laba Subagia <labasubagia22@gmail.com>
2023-06-18 13:19:17 +0800

    basic auth updates

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,10 +1,16 @@
-use actix_web::{http::header::{HeaderMap, self}, web, HttpRequest, HttpResponse, ResponseError};
+use actix_web::{
+    http::header::{self, HeaderMap},
+    web, HttpRequest, HttpResponse, ResponseError,
+};
 use anyhow::Context;
-use reqwest::{StatusCode, header::HeaderValue};
-use secrecy::{Secret, ExposeSecret};
+use argon2::{Argon2, PasswordHash, PasswordVerifier};
+use reqwest::{header::HeaderValue, StatusCode};
+use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
 
-use crate::{domain::SubscriberEmail, email_client::EmailClient};
+use crate::{
+    domain::SubscriberEmail, email_client::EmailClient, telemetry::spawn_blocking_with_tracing,
+};
 
 use super::error_chain_fmt;
 
@@ -40,10 +46,14 @@ impl ResponseError for PublishError {
             PublishError::AuthError(_) => {
                 let mut response = HttpResponse::new(StatusCode::UNAUTHORIZED);
                 let header_value = HeaderValue::from_str(r#"Basic realm="publish""#).unwrap();
-                response.headers_mut().insert(header::WWW_AUTHENTICATE, header_value);
                 response
-            },
-            PublishError::UnexpectedError(_) => HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR),
+                    .headers_mut()
+                    .insert(header::WWW_AUTHENTICATE, header_value);
+                response
+            }
+            PublishError::UnexpectedError(_) => {
+                HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
+            }
         }
     }
 }
@@ -76,8 +86,10 @@ pub async fn publish_newsletter(
                     })?;
             }
             Err(error) => {
-                tracing::warn!(error.cause_chain = ?error, 
-                    "Skipping a confirmed subscriber. Their stored contact details are invalid",)
+                tracing::warn!(
+                    error.cause_chain = ?error,
+                    "Skipping a confirmed subscriber. Their stored contact details are invalid",
+                )
             }
         }
     }
@@ -119,19 +131,70 @@ fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Erro
     })
 }
 
-async fn validate_credentials(credentials: Credentials, pool: &PgPool) ->Result<uuid::Uuid,PublishError> {
-    let user_id = sqlx::query!(
-            r#"SELECT user_id FROM users WHERE username=$1 AND password=$2"#,
-            credentials.username, 
-            credentials.password.expose_secret()
-        )
-        .fetch_optional(pool)
-        .await.context("Failed to perform a query to validate auth credentials.")
+#[tracing::instrument(name = "Validate credentials", skip(credentials, pool))]
+async fn validate_credentials(
+    credentials: Credentials,
+    pool: &PgPool,
+) -> Result<uuid::Uuid, PublishError> {
+    let mut user_id = None;
+    let mut expected_password_hash = Secret::new(
+        "$argon2id$v=19$m=15000,t=2,p=1$gZiV/M1gPc22ElAH/Jh1Hw$CWOrkoo7oJBQ/iyh7uJ0LO2aLEfrHwTWllSAxT0zRno".to_string()
+    );
+
+    if let Some((stored_user_id, stored_password_hash)) =
+        get_stored_credentials(&credentials.username, pool)
+            .await
+            .map_err(PublishError::UnexpectedError)?
+    {
+        user_id = Some(stored_user_id);
+        expected_password_hash = stored_password_hash;
+    }
+
+    spawn_blocking_with_tracing(|| {
+        verify_password_hash(expected_password_hash, credentials.password)
+    })
+    .await
+    .context("Failed to spawn blocking task.")
+    .map_err(PublishError::UnexpectedError)??;
+
+    user_id.ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))
+}
+
+#[tracing::instrument(
+    name = "Verify password hash",
+    skip(expected_password_hash, password_candidate)
+)]
+fn verify_password_hash(
+    expected_password_hash: Secret<String>,
+    password_candidate: Secret<String>,
+) -> Result<(), PublishError> {
+    let expected_password_hash = PasswordHash::new(expected_password_hash.expose_secret())
+        .context("Failed to parse hash in PHC string format.")
         .map_err(PublishError::UnexpectedError)?;
-    user_id
-        .map(|row| row.user_id)
-        .ok_or_else(||anyhow::anyhow!("Invalid username or password"))
+
+    Argon2::default()
+        .verify_password(
+            password_candidate.expose_secret().as_bytes(),
+            &expected_password_hash,
+        )
+        .context("Invalid password")
         .map_err(PublishError::AuthError)
+}
+
+#[tracing::instrument(name = "Get stored credentials", skip(username, pool))]
+async fn get_stored_credentials(
+    username: &str,
+    pool: &PgPool,
+) -> Result<Option<(uuid::Uuid, Secret<String>)>, anyhow::Error> {
+    let row = sqlx::query!(
+        r#"SELECT user_id, password_hash FROM users WHERE username=$1"#,
+        username,
+    )
+    .fetch_optional(pool)
+    .await
+    .context("Failed to perform a query to validate auth credentials.")?
+    .map(|row| (row.user_id, Secret::new(row.password_hash)));
+    Ok(row)
 }
 
 struct ConfirmedSubscriber {
