7dc2b8b95c4c307e4a79a1135679d12d90aa907f
Laba Subagia <labasubagia22@gmail.com>
2023-06-25 22:00:47 +0800

    publish newsletter idempotency

--- src/routes/admin/newsletter/get.rs
+++ src/routes/admin/newsletter/get.rs
@@ -1,10 +1,13 @@
 use actix_web::{http::header::ContentType, HttpResponse};
 use actix_web_flash_messages::IncomingFlashMessages;
 use std::fmt::Write;
+use uuid::Uuid;
 
 pub async fn publish_newsletter_form(
     flash_messages: IncomingFlashMessages,
 ) -> Result<HttpResponse, actix_web::Error> {
+    let idempotency_key = Uuid::new_v4().to_string();
+
     let mut msg_html = String::new();
     for m in flash_messages.iter() {
         writeln!(msg_html, "<p><i>{}</i></p>", m.content()).unwrap();
@@ -24,33 +27,34 @@ pub async fn publish_newsletter_form(
                     {msg_html}    
                     <h1>Publish Newsletter</h1>
                     <form action="/admin/newsletters" method="post">
-                    <label>Title:<br>
-                        <input
-                            type="text"
-                            placeholder="Enter the issue title"
-                            name="title"
-                        >
-                    </label>
-                    <br>
-                    <label>Plain text content:<br>
-                        <textarea
-                            placeholder="Enter the content in plain text"
-                            name="text_content"
-                            rows="20"
-                            cols="50"
-                        ></textarea>
-                    </label>
-                    <br>
-                    <label>HTML content:<br>
-                        <textarea
-                            placeholder="Enter the content in HTML format"
-                            name="html_content"
-                            rows="20"
-                            cols="50"
-                        ></textarea>
-                    </label>
-                    <br>
-                    <button type="submit">Publish</button>
+                        <label>Title:<br>
+                            <input
+                                type="text"
+                                placeholder="Enter the issue title"
+                                name="title"
+                            >
+                        </label>
+                        <br>
+                        <label>Plain text content:<br>
+                            <textarea
+                                placeholder="Enter the content in plain text"
+                                name="text_content"
+                                rows="20"
+                                cols="50"
+                            ></textarea>
+                        </label>
+                        <br>
+                        <label>HTML content:<br>
+                            <textarea
+                                placeholder="Enter the content in HTML format"
+                                name="html_content"
+                                rows="20"
+                                cols="50"
+                            ></textarea>
+                        </label>
+                        <br>
+                        <input hidden type="text" name="idempotency_key" value="{idempotency_key}">
+                        <button type="submit">Publish</button>
                     </form>
                     <p><a href="/admin/dashboard">&lt;- Back</a></p>
                 </body>
